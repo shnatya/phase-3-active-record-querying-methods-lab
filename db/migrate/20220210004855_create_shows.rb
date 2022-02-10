d616722fc3be0e9d39573ba51ec8e117044c7e98
Anastasia Shatokhina <shnatya@gmail.com>
2022-02-09 19:26:45 -0800

    commit tree 78ea01cda7781bfb1684d1be010082d316d5a011

---
+++ db/migrate/20220210004855_create_shows.rb
@@ -0,0 +1,10 @@
+class CreateShows < ActiveRecord::Migration[6.1]
+  def change
+    create_table :shows do |c|
+      c.string :name
+      c.string :network
+      c.string :day
+      c.integer :rating
+    end
+  end
+end
